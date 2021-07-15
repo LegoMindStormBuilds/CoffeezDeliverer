daaa4d3f3600827cef43bceb1d56dc1e1e102682
omegahydrasoftwares0722 <redhydratechnologies@gmail.com>
2021-07-15 12:31:05 -0400

    Update cdv.py

--- src/cdv.py
+++ src/cdv.py
@@ -3,7 +3,13 @@ from mindstorms.control import wait_for_seconds, wait_until, Timer
 from mindstorms.operator import greater_than, greater_than_or_equal_to, less_than, less_than_or_equal_to, equal_to, not_equal_to
 import math
 
-
+#>INFO:
+#
+#
+#
+#
+#
+#
 # Create your objects here.
 cdv = MSHub()
 mm = MotorPair('A','B')
